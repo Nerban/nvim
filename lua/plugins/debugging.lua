8d3fa4707825927af480863b480ac988518f38c3
Nerban <85192213+Nerban@users.noreply.github.com>
2024-10-06 13:36:27 +0200

    Initial commit

---
+++ lua/plugins/debugging.lua
@@ -0,0 +1,36 @@
+return {
+	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = {
+			"mfussenegger/nvim-dap",
+			"nvim-neotest/nvim-nio",
+		},
+	},
+	{
+		"mfussenegger/nvim-dap",
+		config = function()
+			local dap, dapui = require("dap"), require("dapui")
+			dap.listeners.before.attach.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				dapui.close()
+			end
+
+			vim.keymap.set("n", "<F5>", dap.continue)
+			vim.keymap.set("n", "<F10>", dap.step_over)
+			vim.keymap.set("n", "<F11>", dap.step_into)
+			vim.keymap.set("n", "<F12>", dap.step_out)
+			vim.keymap.set("n", "<Leader>b", dap.toggle_breakpoint)
+			vim.keymap.set("n", "<Leader>B", dap.set_breakpoint)
+			vim.keymap.set("n", "<Leader>dr", dap.repl.open)
+			vim.keymap.set("n", "<Leader>dl", dap.run_last)
+		end,
+	},
+}
